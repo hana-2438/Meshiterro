e00b1056543997674890f0fb13fcf095ba8c32c7
EC2 Default User <ec2-user@ip-172-31-11-216.ap-northeast-1.compute.internal>
2023-01-18 08:06:27 +0000

    [Start]4-19

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable#：パスワードをリセット、：ログイン情報を保存、：emailのフォーマットなどのバリデーション
   has_many :post_images, dependent: :destroy
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
 has_one_attached :profile_image
 
